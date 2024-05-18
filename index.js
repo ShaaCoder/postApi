d0c5e728dc6dada524fb8a714b6bad030e9c625b
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-05-19 03:15:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 console.log("Serving static files from:", path.join(__dirname, "/images"));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-app.use(cors({origin:"https://postapi-1.onrender.com/posts",credentials:true}))
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
