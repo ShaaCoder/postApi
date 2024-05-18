bee3cc830ec818153fa50eb0e2e6aba30a9b5d43
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-05-19 01:33:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
     });
     console.log("Database connection established successfully");
   } catch (err) {
