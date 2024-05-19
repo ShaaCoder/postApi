d083bcf380ec5ea86c64077adb8d4f5db349436c
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-05-19 15:45:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ dotenv.config();
 console.log("Serving static files from:", path.join(__dirname, "/images"));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000/', // Replace with your frontend's domain
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
